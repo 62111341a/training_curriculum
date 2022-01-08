f0c6e0d347e76ca1cc8d5843907ffb77234fb403
62111341a <water.melon1341@gmail.com>
2022-01-08 21:08:53 +0900

    lssue7

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -12,7 +12,7 @@ class CalendarsController < ApplicationController
     redirect_to action: :index
   end
 
-  def plan_params.require(:calendars)
+  def plan_params
     params.require(:calendars).permit(:date, :plan)
   end
 
@@ -24,17 +24,19 @@ class CalendarsController < ApplicationController
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
-
+  end
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-
+  
     7.times do |x|
       today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
-      end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
-      @week_days.push(days)
     end
-
-  end
-end
+      wday_num = Date.today.wday
+      if #「wday_numが7以上の場合」という条件式
+        wday_num = wday_num -7
+   end
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+      @week_days.push(days)
+   end
+   end
